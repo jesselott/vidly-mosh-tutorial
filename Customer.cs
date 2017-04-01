150add7f9ad210e3cb6836f0a22ac7edfc92fea7
Jesse Lott <jesse.e.lott@outlook.com>
2017-03-31 22:04:24 -0400

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
+    }
+}
---
+++ MembershipTypeNameEnum.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace Vidly.Models
+{
+    public enum MembershipTypeNameEnum
+    {
+        [Description("Pay as You Go")]
+        Pay_As_You_Go = 1,
+        Weekly = 2,
+        Monthly = 3,
+        Annually = 4
+    };
+}
